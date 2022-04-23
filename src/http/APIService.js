f8bcc6f64f1e66808915029fc480837711d3f0fa
jroeuno <60026699+jroeuno@users.noreply.github.com>
2022-04-23 13:56:35 -0500

    Update APIService.js

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,7 +1,7 @@
   /* eslint-disable */
   import axios from 'axios';
   //const API_URL = 'http://jroeuno.pythonanywhere.com/';
-  const API_URL = 'http://jroeuno.pythonanywhere.com/';
+  const API_URL = 'http://jroeuno.pythonanywhere.com';
   
   export class APIService {
     constructor() {
